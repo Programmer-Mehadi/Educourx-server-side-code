d91d3e87219e4713778bddd1fec7ac293ce6e8a1
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-10-25 22:09:22 +0600

    course path setup

--- index.js
+++ index.js
@@ -12,9 +12,16 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const course = courses.find((course) => course.id === id);
+    res.send(course);
+})
 app.get('/categories', (req, res) => {
     res.send(category);
 })
+
 app.listen(port, () => {
     console.log(`server running port : ${port}`);
 })
