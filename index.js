de883e1ae16d21cb6443f3ba50fed82858ed8020
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-10-25 22:15:11 +0600

    path edit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
-app.get('/courses/:id', (req, res) => {
+app.get('/course/:id', (req, res) => {
     const id = req.params.id;
     console.log(id);
     const course = courses.find((course) => course.id === id);
