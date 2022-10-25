a672ef3d0358e69a24b820938b8ee377c0ed71f0
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-10-25 11:15:46 +0600

    fake data insert and set path

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 
+const courses = require('./data/courses.json');
+const category = require('./data/category.json');
 app.get('/', (req, res) => {
     res.send('server ', )
 })
-
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+app.get('/category', (req, res) => {
+    res.send(category);
+})
 app.listen(port, () => {
     console.log(`server running port : ${port}`);
 })
